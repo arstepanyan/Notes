35a5fba880b495c86a40e070526e5df713556942
Araks Stepanyan <arax.stepanyan@gmail.com>
2019-08-27 12:30:47 -0700

    correct typo

--- programming/elements_prog_interview/10_heaps/merge_sorted_files.py
+++ programming/elements_prog_interview/10_heaps/merge_sorted_files.py
@@ -1,5 +1,5 @@
 '''
-Write a program that takes as input a set of orted sequences and computes the union of these sequences as a sorted sequence.
+Write a program that takes as input a set of sorted sequences and computes the union of these sequences as a sorted sequence.
 For example, if the input is [3, 5, 7], [0, 6], and [0, 6, 28], then the output is [0, 0, 3, 5, 6, 6, 7, 28].
 '''
 import heapq
