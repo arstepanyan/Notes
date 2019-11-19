1ef580c230ac5f3324ef2a557420df57c4651341
Araks Stepanyan <arax.stepanyan@gmail.com>
2019-11-18 19:31:22 -0800

    add doubley linked list solution

--- programming/elements_prog_interview/12_hash_tables/3_implement_isbn_cache.py
+++ programming/elements_prog_interview/12_hash_tables/3_implement_isbn_cache.py
@@ -14,6 +14,7 @@ If ISBN is present, update that entry to be the most recently used ISBN.
 * Erase: remove the specified ISBN and corresponding value from the case. Return true if the ISBN was present; otherwise, return false.
 """
 
+# Ordered dictionary solution
 # Time for lookup and update = O(1)
 import collections
 class ISBMCache:
@@ -41,6 +42,56 @@ class ISBMCache:
     def erase(self, key):
         return self.cache.pop(key, None) is not None
 
+
+
+# Doubley linked list solution
+class Node:
+    def __init__(self, k, v):
+        self.key = k
+        self.val = v
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.dic = dict()
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def get(self, key):
+        if key in self.dic:
+            n = self.dic[key]
+            self._remove(n)
+            self._add(n)
+            return n.val
+        return -1
+
+    def put(self, key, value):
+        if key in self.dic:
+            self._remove(self.dic[key])
+        n = Node(key, value)
+        self._add(n)
+        self.dic[key] = n
+        if len(self.dic) > self.capacity:
+            n = self.head.next
+            self._remove(n)
+            del self.dic[n.key]
+
+    def _remove(self, node):
+        p = node.prev
+        n = node.next
+        p.next = n
+        n.prev = p
+
+    def _add(self, node):
+        p = self.tail.prev
+        p.next = node
+        self.tail.prev = node
+        node.prev = p
+        node.next = self.tail
 
 
 if __name__ == "__main__":
