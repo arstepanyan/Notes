13d3c5d71c8add9922e7d1fd98601addd92fa2af
Araks Stepanyan <arax.stepanyan@gmail.com>
2019-11-20 20:59:52 -0800

    another version of the tower of hanoi and time complexity analysis

--- programming/elements_prog_interview/15_recursion/1_the_tower_of_hanoi.py
+++ programming/elements_prog_interview/15_recursion/1_the_tower_of_hanoi.py
@@ -8,6 +8,13 @@ Write a function which returns a sequence of operations that result in the trans
 Each operation should be encoded as a pair (from_peg, to_peg)
 '''
 
+# Time = O(2^n)
+# Explanation: The number of moves, T(n), satisfies the following recurrence: T(n) = T(n-1) + 1 + T(n-1) = 1 + 2T(n-1).
+#              The first T(n-1) corresponds to the transfer of the n-1 rings from P1 to P2,
+#              and the second T(n-1) corresponds to the transfer from P3 to P2. This recurrence solves to T(n) = 2^n - 1.
+#              One way to see this is to "unwrap" the recurrence: T(n) = 1 + 2 + 4 + ... + 2^k T(n-k).
+#              Printing a single move takes O(1) time, so the time complexity is O(2^n)
+
 
 def tower_of_hanoi(fr, to, spare, n):
     res = []
@@ -22,9 +29,29 @@ def _tower_of_hanoi(n, fr, to, spare, res):
         _tower_of_hanoi(n - 1, spare, to, fr, res)
 
 
+def tower_of_hanoi_2(fr, to, spare, n):
+    res = []
+    _tower_of_hanoi_2(n, fr, to, spare, res)
+    return res
+
+
+def _tower_of_hanoi_2(n, fr, to, spare, res):
+    if n == 1:
+        res.append([fr, to])
+    else:
+        _tower_of_hanoi_2(n - 1, fr, spare, to, res)
+        _tower_of_hanoi_2(1, fr, to, spare, res)
+        _tower_of_hanoi_2(n - 1, spare, to, fr, res)
+
+
 if __name__ == "__main__":
     peg1, peg2, peg3 = 'fr', 'to', 'spare'
     print(tower_of_hanoi(peg1, peg2, peg3, 1))
     print(tower_of_hanoi(peg1, peg2, peg3, 2))
     print(tower_of_hanoi(peg1, peg2, peg3, 3))
     print(tower_of_hanoi(peg1, peg2, peg3, 4))
+
+    print(tower_of_hanoi_2(peg1, peg2, peg3, 1))
+    print(tower_of_hanoi_2(peg1, peg2, peg3, 2))
+    print(tower_of_hanoi_2(peg1, peg2, peg3, 3))
+    print(tower_of_hanoi_2(peg1, peg2, peg3, 4))
