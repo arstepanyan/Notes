d2c498b56a9691bd876c8b553d0742e1245cca63
Araks Stepanyan <arax.stepanyan@gmail.com>
2020-02-25 14:41:54 -0800

    implement h index

---
+++ programming/elements_prog_interview/13_sorting/3_compute_h_index.py
@@ -0,0 +1,19 @@
+"""
+Given an array of positive integers, find the largest h
+such that there are at least h entries in the array that are greater than or equal to h.
+"""
+
+# Time Complexity = O(n log n)
+def h_index(arr):
+    len_arr = len(arr)
+    arr.sort()
+    for i in range(len_arr):
+        if arr[i] >= len_arr - i:
+            return len_arr - i
+    return False
+
+
+if __name__ == '__main__':
+    nums = [1, 7, 9, 2, 5]
+    print(h_index(nums))
+    print(h_index2(nums))
